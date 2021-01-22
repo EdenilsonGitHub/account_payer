738bce278e04c8e99f9a8982605b4d6d78d512bc
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-01-21 22:19:02 -0300

    AP-5 - Criação do index do usuário, adicionado logo, configurado cor padrão e alterado layout da nav

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -12,8 +12,12 @@ function home() {
     $(window).on("scroll", function() {
         if($(window).scrollTop()) {
             $('nav').addClass('black');
+            $('nav').addClass('border_header');
+            $('.fundo_logo').addClass('fundo_logo_border_topo');
         } else {
             $('nav').removeClass('black');
+            $('nav').removeClass('border_header');
+            $('.fundo_logo').removeClass('fundo_logo_border_topo');
         }
     })
 }
